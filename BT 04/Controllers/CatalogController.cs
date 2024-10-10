4f387b8e1775047afa925095fe0315e0bbc6de82
YA DAT <22520243@gm.uit.edu.vn>
2024-10-10 14:19:32 +0700

    hoanthanhbai4

---
+++ BT 04/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using BT_04.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT_04.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            string connectionstring = "Server=localhost;Database=QuanLySanPham;Integrated Security=True;";
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connectionstring);
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ BT 04/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using BT_04.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT_04.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            string connectionstring = "Server=localhost;Database=QuanLySanPham;Integrated Security=True;";
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connectionstring);
+            List<Product> dsProduct = context.Products.ToList();
+            return View(dsProduct);
+        }
+    }
+}
